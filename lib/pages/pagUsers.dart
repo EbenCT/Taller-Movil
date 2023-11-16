0e69e621b1b89ca23c372a91253b305f329c367d
EbenCT <cayoeben64@gmail.com>
2023-11-16 17:54:26 -0400

    Conectando datos del cliente

--- lib/pages/pagUsers.dart
+++ lib/pages/pagUsers.dart
@@ -1,15 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:proy1/Service/AuthService.dart';
 
 var bandera = true;
 
 class PaginaUsers extends StatefulWidget {
-  const PaginaUsers({super.key});
+  const PaginaUsers({Key? key}) : super(key: key);
 
   @override
   State<PaginaUsers> createState() => _PaginaUsersState();
 }
 
 class _PaginaUsersState extends State<PaginaUsers> {
+  final AuthService _authService = AuthService();
+  Map<String, dynamic>? _userData;
+
+  @override
+  void initState() {
+    super.initState();
+    _loadUserData();
+  }
+
+  Future<void> _loadUserData() async {
+    final userData = await _authService.getClientById();
+    setState(() {
+      _userData = userData['data'];
+      print(_userData);
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
     return ListView(
       children: [
@@ -22,164 +41,75 @@ class _PaginaUsersState extends State<PaginaUsers> {
             ),
           ),
         ),
-        Container(
-          child: Image.network(
-              height: 120,
-              "https://i.pinimg.com/originals/6f/57/76/6f57760966a796644b8cfb0fbc449843.png"),
-        ),
-        nombreUsers(),
-        CiUs(),
-        CiUSers(),
-        TelfUs(),
-        TelfUsers(),
-        CorreoUs(),
-        CorreoUsers(),
-        DirUs(),
-        DirUSers(),
-        SizedBox(
-          height: 20,
-        ),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            BotEditar(context),
-            SizedBox(
-              width: 30,
-            ),
-            BotGuardar(context)
-          ],
-        )
+        if (_userData != null) ...[
+          nombreUsers('Nombre: ${_userData!['nombre']} ${_userData!['apellido']}'), // Accede a 'nombre' y 'apellido' dentro de '_userData'
+          userDataField('ID', _userData!['id'].toString()), // Accede a 'id' dentro de '_userData'
+          userDataField('Teléfono', _userData!['telefono']), // Accede a 'telefono' dentro de '_userData'
+          userDataField('Dirección', _userData!['direccion']), // Accede a 'direccion' dentro de '_userData'
+          userDataField('Género', _userData!['genero']), // Accede a 'genero' dentro de '_userData'
+          SizedBox(height: 20),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              BotEditar(),
+              SizedBox(width: 30),
+              BotGuardar(),
+            ],
+          ),
+        ],
       ],
     );
   }
 
-  Widget nombreUsers() {
+  Widget nombreUsers(String nombre) {
     return Center(
       child: Container(
-        child: Text("Juan pablo", style: TextStyle(fontSize: 20)),
+        child: Text(nombre, style: TextStyle(fontSize: 20)),
       ),
     );
   }
 
-  Widget CiUs() {
-    return Center(
-      child: Container(
-        width: 300,
-        child: Text("CI:", style: TextStyle(fontSize: 20)),
-      ),
-    );
-  }
-
-  Widget CiUSers() {
-    return Container(
-      padding: EdgeInsets.symmetric(horizontal: 30, vertical: 5),
-      child: TextField(
-        readOnly: bandera,
-        decoration: InputDecoration(
-          hintText: "8125398",
-          fillColor: Colors.white,
-          filled: true,
-        ),
-      ),
-    );
-  }
-
-  Widget TelfUs() {
+  Widget userDataField(String label, String value) {
     return Center(
       child: Container(
         width: 300,
-        child: Text("Teléfono:", style: TextStyle(fontSize: 20)),
+        child: Text("$label: $value", style: TextStyle(fontSize: 20)),
       ),
     );
   }
 
-  Widget TelfUsers() {
-    return Container(
-      padding: EdgeInsets.symmetric(horizontal: 30, vertical: 5),
-      child: TextField(
-        readOnly: bandera,
-        decoration: InputDecoration(
-          hintText: "73625259",
-          fillColor: Colors.white,
-          filled: true,
-        ),
-      ),
-    );
-  }
-
-  Widget CorreoUs() {
-    return Center(
-      child: Container(
-        width: 300,
-        child: Text("Correo:", style: TextStyle(fontSize: 20)),
-      ),
-    );
-  }
-
-  Widget CorreoUsers() {
-    return Container(
-      padding: EdgeInsets.symmetric(horizontal: 30, vertical: 5),
-      child: TextField(
-        readOnly: bandera,
-        decoration: InputDecoration(
-          hintText: "juanpa@gmial.com",
-          fillColor: Colors.white,
-          filled: true,
-        ),
-      ),
-    );
-  }
-
-  Widget DirUs() {
-    return Center(
-      child: Container(
-        width: 300,
-        child: Text("Dirección:", style: TextStyle(fontSize: 20)),
-      ),
-    );
-  }
-
-  Widget DirUSers() {
-    return Container(
-      padding: EdgeInsets.symmetric(horizontal: 30, vertical: 5),
-      child: TextField(
-        readOnly: bandera,
-        decoration: InputDecoration(
-          hintText: "Av. Alemana 5to anillo",
-          fillColor: Colors.white,
-          filled: true,
-        ),
-      ),
-    );
-  }
-
-  Widget BotEditar(BuildContext context) {
+  Widget BotEditar() {
     return TextButton(
-        style: ButtonStyle(
-            backgroundColor: MaterialStatePropertyAll(Colors.blueAccent)),
-        onPressed: () {
-          setState(() {
-            bandera = false;
-          });
-        },
-        child: Text(
-          "Editar",
-          style: TextStyle(color: Colors.white, fontSize: 20),
-        ));
+      style: ButtonStyle(
+        backgroundColor: MaterialStatePropertyAll(Colors.blueAccent),
+      ),
+      onPressed: () {
+        setState(() {
+          // Habilitar edición
+        });
+      },
+      child: Text(
+        "Editar",
+        style: TextStyle(color: Colors.white, fontSize: 20),
+      ),
+    );
   }
 
-  Widget BotGuardar(BuildContext context) {
+  Widget BotGuardar() {
     return TextButton(
-        style: ButtonStyle(
-            backgroundColor: MaterialStatePropertyAll(Colors.blueAccent)),
-        onPressed: () {
-          setState(() {
-            bandera = true;
-          });
-        },
-        child: Text(
-          "Guardar",
-          style: TextStyle(color: Colors.white, fontSize: 20),
-        ));
+      style: ButtonStyle(
+        backgroundColor: MaterialStatePropertyAll(Colors.blueAccent),
+      ),
+      onPressed: () {
+        setState(() {
+          // Guardar cambios
+        });
+      },
+      child: Text(
+        "Guardar",
+        style: TextStyle(color: Colors.white, fontSize: 20),
+      ),
+    );
   }
 }
+
