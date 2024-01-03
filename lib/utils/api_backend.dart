36d3664e64bcc3592dee8f70032a1971491bcc36
EbenCT <cayoeben64@gmail.com>
2024-01-03 17:43:29 -0300

    mis cambios

--- lib/utils/api_backend.dart
+++ lib/utils/api_backend.dart
@@ -1,4 +1,4 @@
-//const apiBackend = "18.220.60.49/api"; 
-// const apiBackend = "192.168.0.2:8000/api";
-const apiBackend = "3.144.128.157/api"; 
-const apiHost = "3.144.128.157"; 
+const apiBackend = "10.0.2.2:8000/api"; 
+const apiHost = "10.0.2.2:8000/api";
+//const apiBackend = "3.144.128.157/api"; 
+//const apiHost = "3.144.128.157"; 
